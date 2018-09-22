efba162d05116968dfcd982c070f15c3ef1f88ab
leomichelman96 <42454974+leomichelman96@users.noreply.github.com>
2018-09-22 14:30:29 -0300

    Update 9-Diagnosticos de Residuos- Parte 2.R

--- 9-Diagnosticos de Residuos- Parte 2.R
+++ 9-Diagnosticos de Residuos- Parte 2.R
@@ -1,6 +1,6 @@
 
 
-#Aula 9 - Diagnósticos de Resíduos 2
+#Aula 9 - DiagnÃ³sticos de ResÃ­duos 2
 install.packages("lmtest")
 install.packages("aTSA")
 library(lmtest)
@@ -13,18 +13,24 @@ library(readxl)
 
 data(jocci)                                                       #Department of commerce commodity price index - USA
 View(jocci)
-JOCCI <- as.data.frame(jocci)                                     #Y é o Index Jocci . dy é a variação (expressa pela diferença) em logaritmos.
+JOCCI <- as.data.frame(jocci)                                     #Y Ã© o Index Jocci . dy Ã© a variaÃ§Ã£o (expressa pela diferenÃ§a) em logaritmos.
 joccits <- ts(JOCCI$dy,start = 1959,frequency = 12)
-plot(joccits, main="Índice Jocci", xlab="Ano", ylab="Indice")
+plot(joccits, main="Ãndice Jocci", xlab="Ano", ylab="Indice")
 
 #Definindo Formato dos Modelos
 ar6model <- dy~dy1+dy2+dy3+dy4+dy5+dy6
-ar5model 
-ar4model 
-ar3model 
-ar2model 
-ar1model 
+ar5model <- dy~dy1+dy2+dy3+dy4+dy5
+ar4model <- dy~dy1+dy2+dy3+dy4
+ar3model <- dy~dy1+dy2+dy3
+ar2model <- dy~dy1+dy2
+ar1model <- dy~dy1
 
+ar6model <- as.data.frame(jocci)
+ar5model <- as.data.frame(jocci)
+ar4model <- as.data.frame(jocci)
+ar3model <- as.data.frame(jocci)
+ar2model <- as.data.frame(jocci)
+ar1model <- as.data.frame(jocci)
 #Executando os Teste LM-Breuch-Godfrey
 
 TesteBGAR6 <- bgtest(ar6model,data=jocci)
@@ -67,7 +73,7 @@ View(Resultados)
 
 
 #Carregando o arquivo xls
-variacao_PIB <- read.table("c:/Econometria/variacao.xls", header = T)                 #Lê o arquivo variacao.xls na pasta c:/Econometria
+variacao_PIB <- read.table("c:/Econometria/variacao.xls", header = T)                 #LÃª o arquivo variacao.xls na pasta c:/Econometria
 variacao_PIB <- as.data.frame(variacao_PIB[,-1])                                      #Apaga a primeira coluna
 
 
